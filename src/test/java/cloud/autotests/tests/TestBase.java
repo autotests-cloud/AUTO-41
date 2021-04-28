4b4367a781c1c0cd933e1dd045f83f2314bbc330
John Smoker <testgits@yandex.ru>
2021-04-29 00:07:11 +0500

    fixed

--- src/test/java/cloud/autotests/tests/TestBase.java
+++ src/test/java/cloud/autotests/tests/TestBase.java
@@ -1,8 +1,9 @@
 package cloud.autotests.tests;
 
+import com.codeborne.selenide.Configuration;
 import io.qameta.allure.junit5.AllureJunit5;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static cloud.autotests.helpers.AttachmentsHelper.*;
@@ -11,9 +12,14 @@ import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 @ExtendWith({AllureJunit5.class})
 public class TestBase {
-    @BeforeAll
-    static void setUp() {
+    public static final String baseUrl = "https://the-internet.herokuapp.com";
+    public static final String username = "tomsmith";
+    public static final String password = "SuperSecretPassword!";
+
+    @BeforeEach
+    void setUp() {
         configureDriver();
+        Configuration.baseUrl = baseUrl;
     }
 
     @AfterEach
